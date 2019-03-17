d8e3a249fdc79a58f37c0cd16f3d244d4e966aef
avihay <avihay@intezer.com>
2019-03-17 17:27:25 +0200

    feat(SDK): create basic index SDK

--- tests/unit/test_index.py
+++ tests/unit/test_index.py
@@ -1,10 +1,7 @@
-import sys
-
 import responses
 
 from intezer_sdk import consts
 from intezer_sdk import errors
-from intezer_sdk.api import set_global_api
 from intezer_sdk.index import Index
 from tests.unit.base_test import BaseTest
 
@@ -18,16 +15,7 @@ except ImportError:
 
 class IndexSpec(BaseTest):
     def setUp(self):
-        self.full_url = consts.BASE_URL + consts.API_VERSION
-        consts.CHECK_STATUS_INTERVAL = 0
-
-        # Python 2 support
-        if sys.version_info[0] < 3:
-            self.patch_prop = '__builtin__.open'
-        else:
-            self.patch_prop = 'builtins.open'
-
-        set_global_api()
+        super(IndexSpec, self).setUp()
 
     def test_index_malicious_without_family_name_raise_value_error(self):
         # Act + Assert
