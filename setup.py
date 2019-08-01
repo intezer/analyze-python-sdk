0e1ad240ce60d9ed7e9e7270e3df803a70fbdea9
shaytidhar <shaytidhar@gmail.com>
2019-08-01 12:22:48 +0300

    chore: remove python27 from classifiers

--- setup.py
+++ setup.py
@@ -37,9 +37,8 @@ setup(
         'pytest == 3.6.4',
         'mock == 2.0.0'
     ],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     classifiers=[
-        'Programming Language :: Python :: 2.7'
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7']
