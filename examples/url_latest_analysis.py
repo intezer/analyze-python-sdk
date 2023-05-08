f198fac5135d74dac59de6f4c8bee31b7bb0ee67
Matan Yechiel <matany90>
2023-05-08 17:13:48 +0300

    remove creds

--- examples/url_latest_analysis.py
+++ examples/url_latest_analysis.py
@@ -5,7 +5,7 @@ from intezer_sdk.analysis import UrlAnalysis
 
 
 def get_url_latest_analysis(url: str):
-    api.set_global_api('519643d2-f373-40c1-9616-d4650c4741ee')
+    api.set_global_api('<api-key>')
     analysis = UrlAnalysis.from_latest_analysis(url)
     pprint(analysis.result())
 
