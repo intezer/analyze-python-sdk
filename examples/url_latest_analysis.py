51e6f3c8a47c4235e1f966cbea03a68c7ac1ddff
Matan Yechiel <matany90>
2023-05-08 17:56:07 +0300

    add tests and examples

--- examples/url_latest_analysis.py
+++ examples/url_latest_analysis.py
@@ -8,7 +8,8 @@ from intezer_sdk.analysis import UrlAnalysis
 def get_url_latest_analysis(url: str):
     api.set_global_api('<api-key>')
     analysis = UrlAnalysis.from_latest_analysis(url)
-    pprint(analysis.result())
+    if analysis:
+        pprint(analysis.result())
 
 if __name__ == '__main__':
     get_url_latest_analysis(*sys.argv[1:])
