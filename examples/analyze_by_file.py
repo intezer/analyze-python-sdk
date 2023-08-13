067b57cabad2b5aef7212cdd1fd2e6c251095f70
Guy Salomon <guy@intezer.com>
2023-08-13 12:35:34 +0300

    Fix: is_available

--- examples/analyze_by_file.py
+++ examples/analyze_by_file.py
@@ -1,4 +1,3 @@
-import io
 import sys
 from pprint import pprint
 
@@ -13,16 +12,13 @@ def send_file_with_wait(file_path):
     pprint(analysis.result())
 
 
-def send_file_without_wait():
-    api.set_global_api('2d0caeb6-b5e0-4d36-b8fd-13b5ac4e39f4')
-    analysis = FileAnalysis(file_stream=io.BytesIO(b'dsadsagt432'), file_name='123')
+def send_file_without_wait(file_path):
+    api.set_global_api('<api_key>')
+    analysis = FileAnalysis(file_path=file_path)
     analysis.send()
-    try:
-        data = FileAnalysis.from_latest_hash_analysis('71e0b5e3a3b40b1904bb0fbb412ef7735484efa1c53c946e155f3e6970a1aa71')
-    except Exception as e:
-        print(e)
-    pprint(data)
+    analysis.wait_for_completion()
+    pprint(analysis.result())
 
 
 if __name__ == '__main__':
-    send_file_without_wait()
+    send_file_with_wait(*sys.argv[1:])
