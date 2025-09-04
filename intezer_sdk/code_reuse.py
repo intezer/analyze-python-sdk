681c3fa93c3c869cc0b57a79637b4b2c09c80b4e
Joakim Kennedy <joakim@intezer.com>
2025-09-04 11:36:59 +0100

    Address PR comments
    - Format import statements
    - Use @dataclass for Block class
    - Make is_common a property
    - Switch from " strings to ' strings
    - Use of HTTPStatus instead of int
    - Changed variable format to long-name

--- intezer_sdk/code_reuse.py
+++ intezer_sdk/code_reuse.py
@@ -1,43 +1,48 @@
+from dataclasses import dataclass
+from http import HTTPStatus
 from time import sleep
-from typing import Optional, List
+from typing import Optional
+from typing import List
 
 from intezer_sdk import api
 from intezer_sdk.api import IntezerApiClient
 from intezer_sdk._api import IntezerApi
 
-
+@dataclass
 class Block:
-    def __init__(self, address: int, software_type: str, families: List[str]):
-        self.address = address
-        self.software_type = software_type
-        self.families = families
-        self.is_common = software_type == "common"
+    address: int
+    software_type: str
+    families: List[str]
+
+    @property
+    def is_common(self):
+        return self.software_type == 'common'
 
 
 class CodeReuse:
-    """
+    '''
     Get code reuse for a file.
 
     Parameters:
         api_client (Optional[IntezerApiClient]): An optional Intezer API client instance. If not provided, the global
         API client will be used.
-    """
+    '''
 
     def __init__(self, api_client: Optional[IntezerApiClient] = None):
         self._api = IntezerApi(api_client or api.get_global_api())
 
     def _get_result_from_task(self, result_url: str):
         response = self._api.api.request_with_refresh_expired_access_token(
-            "GET", result_url)
-        while response.status_code == 202:
+            'GET', result_url)
+        while response.status_code == HTTPStatus.ACCEPTED:
             sleep(2)
             response = self._api.api.request_with_refresh_expired_access_token(
-                "GET", result_url)
+                'GET', result_url)
         response.raise_for_status()
         return response.json()['result']
 
     def get_code_blocks(self, sha256: str) -> List[Block]:
-        """
+        '''
         Retrieves a report containing information about reused code blocks for the given SHA-256 hash.
 
         Parameters:
@@ -45,13 +50,13 @@ class CodeReuse:
 
         Returns:
             List[Block]: A sorted list of Block objects representing the code blocks found in the analysis.
-        """
+        '''
         result_url = self._api.get_code_reuse_by_code_block(sha256)
         # This endpoint acts different. We don't get a status and instead have to use
         # the HTTP status code to wait for the report.
         result = self._get_result_from_task(result_url)
         blocks: list[Block] = []
-        for addr, val in result["blocks"].items():
+        for address, block in result['blocks'].items():
             blocks.append(
-                Block(int(addr), val["software_type"], val["code_reuse"]))
+                Block(int(address), block['software_type'], block['code_reuse']))
         return sorted(blocks, key=lambda b: b.address)
